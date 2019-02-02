a9d60573a6e8c582b9c19a78adea7542e72c8bcf
kunnapatt <folk_qq@hotmail.com>
2019-02-02 14:39:51 +0700

    add template

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   
 })
 
-app.post('/webhook', middleware(config), (req, res) => {
+app.post('/webhook', middleware(config), (req, res) => { // req = require to bot | res = response to user
   res.send('Hello!!')
   const event = req.body.events[0];
 
@@ -28,10 +28,49 @@ app.post('/webhook', middleware(config), (req, res) => {
         stickerId: message.stickerId ,
         packageId: message.packageId
       })
-    }else {
+    } else if ( message.type === 'template'){
+      client.replyMessage(event.replyToken, 
+        {
+          "type": "template",
+          "altText": "This is a buttons template",
+          "template": {
+              "type": "buttons",
+              "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover",
+              "imageBackgroundColor": "#FFFFFF",
+              "title": "Menu",
+              "text": "Please select",
+              "defaultAction": {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com/"
+              },
+              "actions": [
+                  {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=123"
+                  },
+                  {
+                    "type": "message",
+                    "label": "Add to cart",
+                    "text": "no no no"
+                  },
+                  {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://google.com"
+                  }
+              ]
+          }
+        })
+    }
+
+    } else {
       client.replyMessage(event.replyToken, {
         type: 'text',
-        text: message.type
+        text: message.type // object message | message.text show text | message.type show type
       })
     }
   }
