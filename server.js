814312731da180e9959f09176967bb08d48b5de4
kunnapatt <folk_qq@hotmail.com>
2019-02-02 15:18:13 +0700

    add carousel

--- server.js
+++ server.js
@@ -29,46 +29,94 @@ app.post('/webhook', middleware(config), (req, res) => { // req = require to bot
         packageId: message.packageId
       })
     } else {
-      client.replyMessage(event.replyToken, 
-        {
-          "type": "template",
-          "altText": "This is a buttons template",
-          "template": {
-              "type": "buttons",
-              "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/1084270206938099712/qR9TdPQD_400x400.jpg",
-              "imageAspectRatio": "rectangle",
-              "imageSize": "cover",
-              "imageBackgroundColor": "#00FDFD",
-              "title": "Kunnapat Thippayapalaphonkul",
-              "text": "Please select",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://google.com/"
-              },
-              "actions": [
-                  {
-                    "type": "datetimepicker",
-                    "label": "Select date",
-                    "data": "stroeId=12345",
-                    "mode": "datetime",
-                    "inital": "2019-02-02t00:00",
-                    "max":"2025-01-01t23:59",
-                    "min":"2019-02-02t00:00"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "Facebook",
-                    "uri": "https://www.facebook.com/folk.kunnapat"
-                  },
-                  {
-                    "type": "uri",
-                    "label": "CPE",
-                    "uri": "http://cpe.eng.cmu.ac.th/2013/"
-                  }
-              ]
-          }
-        })
+      // client.replyMessage(event.replyToken, 
+      //   {
+      //     "type": "template",
+      //     "altText": "This is a buttons template",
+      //     "template": {
+      //         "type": "buttons",
+      //         "thumbnailImageUrl": "https://pbs.twimg.com/profile_images/1084270206938099712/qR9TdPQD_400x400.jpg",
+      //         "imageAspectRatio": "rectangle",
+      //         "imageSize": "cover",
+      //         "imageBackgroundColor": "#00FDFD",
+      //         "title": "Kunnapat Thippayapalaphonkul",
+      //         "text": "Please select",
+      //         "defaultAction": {
+      //             "type": "uri",
+      //             "label": "View detail",
+      //             "uri": "http://google.com/"
+      //         },
+      //         "actions": [
+      //             {
+      //               "type": "datetimepicker",
+      //               "label": "Select date",
+      //               "data": "stroeId=12345",
+      //               "mode": "datetime",
+      //               "inital": "2019-02-02t00:00",
+      //               "max":"2025-01-01t23:59",
+      //               "min":"2019-02-02t00:00"
+      //             },
+      //             {
+      //               "type": "uri",
+      //               "label": "Facebook",
+      //               "uri": "https://www.facebook.com/folk.kunnapat"
+      //             },
+      //             {
+      //               "type": "uri",
+      //               "label": "CPE",
+      //               "uri": "http://cpe.eng.cmu.ac.th/2013/"
+      //             }
+      //         ]
+      //     }
+      //   })
+      client.replyMessage(event.replyToken, {
+        "type": "template",
+        "altText": "this is a carousel template",
+        "template": {
+            "type": "carousel",
+            "columns": [
+                {
+                  "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                  "imageBackgroundColor": "#FFFFFF",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                      {  
+                          "type":"cameraRoll",
+                          "label":"Camera roll"
+                      },
+                      {  
+                        "type":"location",
+                        "label":"Location"
+                     }
+                  ]
+                },
+                {
+                  "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
+                  "imageBackgroundColor": "#000000",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                    {
+                      "type":"datetimepicker",
+                      "label":"Select date",
+                      "data":"storeId=12345",
+                      "mode":"datetime",
+                      "initial":"2017-12-25t00:00",
+                      "max":"2018-01-24t23:59",
+                      "min":"2017-12-25t00:00"
+                    },
+                    {  
+                      "type":"camera",
+                      "label":"Camera"
+                   }
+                ]
+                }
+            ],
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover"
+        }
+      })
     }
   }
 })
