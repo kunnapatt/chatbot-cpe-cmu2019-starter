a52a0eed1a4b7972fabd1b78abc0d33c0b776a28
kunnapatt <folk_qq@hotmail.com>
2019-02-02 11:10:21 +0700

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
-
+const middleware = require('@line/bot-sdk').middleware
 const app = express()
+
+const config = {
+  channelAccessToken: 'V3PpZvmuEE3IVUIrRdbmHYhSGXPOZhUDLxL5ZNucw1jVbKf/iXQ+hUT2whGv/JeP/r8Qlldgj5EyrHRoEXNDtogRqW8kaA9gl49dQMKu0Q+Y+vutvUsTCqnaOj1Hob0bT7tSxCqLJh2TdW7ufMAuLQdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '9700cb174a737762aeab9d2e7a19132f'
+}
 
 app.get('/', function (req, res) {
     res.send('Hello World!!')
+})
+
+app.post('/webhook', middleware(config), (req, res) => {
+  console.log('webhook success')
 })
 
 app.set('port', (process.env.PORT || 4000))
