b7ac92d758c3a62886f24227b667d5ce7fad110d
kunnapatt <folk_qq@hotmail.com>
2019-02-02 11:56:41 +0700

    add event to app.post

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const middleware = require('@line/bot-sdk').middleware
 const app = express()
+const Client = require('@line/bot-sdk').Client;
 
 const config = {
   channelAccessToken: 'V3PpZvmuEE3IVUIrRdbmHYhSGXPOZhUDLxL5ZNucw1jVbKf/iXQ+hUT2whGv/JeP/r8Qlldgj5EyrHRoEXNDtogRqW8kaA9gl49dQMKu0Q+Y+vutvUsTCqnaOj1Hob0bT7tSxCqLJh2TdW7ufMAuLQdB04t89/1O/w1cDnyilFU=',
   channelSecret: '9700cb174a737762aeab9d2e7a19132f'
 }
 
+const client = new Client(config)
+
 app.get('/', function (req, res) {
-    res.send('Hello World!!')
+  res.send('Hello World!!')
+  
 })
 
 app.post('/webhook', middleware(config), (req, res) => {
   res.send('Hello!!')
+  const event = req.body.events[0];
+
+  if (event.type === 'message') {
+    const message = event.message;
+    client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: message,
+    })
+  }
 })
 
 app.set('port', (process.env.PORT || 4000))
